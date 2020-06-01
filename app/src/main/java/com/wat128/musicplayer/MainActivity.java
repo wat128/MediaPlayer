28da8c0b9cca562e7e64fa2b9a52de8ff89fd83f
wat128 <wat128sideb@gmail.com>
2020-06-01 18:29:13 +0900

    音楽の再生・停止

--- app/src/main/java/com/wat128/musicplayer/MainActivity.java
+++ app/src/main/java/com/wat128/musicplayer/MainActivity.java
@@ -2,13 +2,45 @@ package com.wat128.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playBtn;
+    private SeekBar positionBar;
+    private SeekBar volumeBar;
+    private TextView elapsedTimeLabel;
+    private TextView remainingTimeLabel;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playBtn = findViewById(R.id.playBtn);
+        elapsedTimeLabel = findViewById(R.id.elapsedTimeLabel);
+        remainingTimeLabel = findViewById(R.id.remainingTimeLabel);
+
+        // initialized Media Player
+        mp = MediaPlayer.create(this, R.raw.music);
+        mp.setLooping(true);
+        mp.seekTo(0);
+        mp.setVolume(0.5f, 0.5f);
+    }
+
+    public void playBtnClick(View view){
+        if(!mp.isPlaying()) {
+            mp.start();
+            playBtn.setBackgroundResource(R.drawable.stop);
+        } else {
+            mp.pause();
+            playBtn.setBackgroundResource(R.drawable.play);
+        }
     }
 }
